210756cb10c1da85ae7a2333530c1fd9c5667c2a
Jaqueline De Carvalho LaurentI <jaqueline.carvalho@linx.com.br>
2019-10-08 14:03:55 -0300

    Ajuste na calculadora

--- JavaFiap2019/src/DesafioCalculadora/Calculadora.java
+++ JavaFiap2019/src/DesafioCalculadora/Calculadora.java
@@ -6,27 +6,33 @@ public class Calculadora implements CalculadoraBasica{
 
 	@Override
 	public float somar(double op1, double op2) {
-		// TODO Auto-generated method stub
-		return 0;
+		float resultado;
+		resultado = (float) (op1+op2);
+		return resultado;
 	}
 
 	@Override
 	public float subtrair(double op1, double op2) {
-		// TODO Auto-generated method stub
-		return 0;
+		float resultado;
+		resultado = (float) (op1-op2);
+		return resultado;
 	}
 
 	@Override
 	public float dividir(double op1, double op2) {
-		// TODO Auto-generated method stub
-		return 0;
+		float resultado;
+		resultado = (float) (op1*op2);
+		return resultado;
 	}
 
 	@Override
 	public float multiplicar(double op1, double op2) {
-		// TODO Auto-generated method stub
-		return 0;
+		float resultado;
+		resultado = (float) (op1/op2);
+		return resultado;
 	}
 	
-	
+	public void mostrarResultado(float resultado, String operacao) {
+		JOptionPane.showMessageDialog(null, "O resultado da " + operacao + " é de: " + resultado);	
+	}
 }
--- JavaFiap2019/src/DesafioCalculadora/TestaCalculadora.java
+++ JavaFiap2019/src/DesafioCalculadora/TestaCalculadora.java
@@ -15,65 +15,73 @@ public class TestaCalculadora {
 		int i = 0;
 		while (i == 0) {
 			n1 = JOptionPane.showInputDialog("Digite o primeiro número:");
-			if (!n1.matches("^[a-zA-ZÁÂÃÀÇÉÊÍÓÔÕÚÜáâãàçéêíóôõúü]*$")) {
-				operacao = JOptionPane.showInputDialog("Entre com a Operação: ");
+			if (!n1.matches(
+					"^[a-zA-ZÃƒï¿½Ãƒâ€šÃƒÆ’Ãƒâ‚¬Ãƒâ€¡Ãƒâ€°ÃƒÅ Ãƒï¿½Ãƒâ€œÃƒâ€�Ãƒâ€¢ÃƒÅ¡ÃƒÅ“ÃƒÂ¡ÃƒÂ¢ÃƒÂ£ÃƒÂ ÃƒÂ§ÃƒÂ©ÃƒÂªÃƒÂ­ÃƒÂ³ÃƒÂ´ÃƒÂµÃƒÂºÃƒÂ¼]*$")) {
+				operacao = JOptionPane.showInputDialog("Entre com a Operação");
+				if (operacao.contains("*") || operacao.contains("/") || operacao.contains("+")
+						|| operacao.contains("-")) {
+					n2 = JOptionPane.showInputDialog("Digite o segundo número:");
+					if (!n2.matches(
+							"^[a-zA-ZÃƒï¿½Ãƒâ€šÃƒÆ’Ãƒâ‚¬Ãƒâ€¡Ãƒâ€°ÃƒÅ Ãƒï¿½Ãƒâ€œÃƒâ€�Ãƒâ€¢ÃƒÅ¡ÃƒÅ“ÃƒÂ¡ÃƒÂ¢ÃƒÂ£ÃƒÂ ÃƒÂ§ÃƒÂ©ÃƒÂªÃƒÂ­ÃƒÂ³ÃƒÂ´ÃƒÂµÃƒÂºÃƒÂ¼]*$")) {
 
-				n2 = JOptionPane.showInputDialog("Digite o segundo número:");
-				if (!n2.matches("^[a-zA-ZÁÂÃÀÇÉÊÍÓÔÕÚÜáâãàçéêíóôõúü]*$")) {
-					if (operacao.equals("+")) {
-						resultado = cb.somar(Double.parseDouble(n1), Double.parseDouble(n2));
-						cb.mostrarResultado((float) resultado, "Soma");
+						if (operacao.equals("+")) {
+							resultado = cb.somar(Double.parseDouble(n1), Double.parseDouble(n2));
+							cb.mostrarResultado((float) resultado, "Soma");
 
-						int option = JOptionPane.showConfirmDialog(null, "Deseja repetir a operação?", "Aviso",
-								JOptionPane.YES_NO_OPTION);
+							int option = JOptionPane.showConfirmDialog(null, "Deseja repetir a Operação", "Aviso",
+									JOptionPane.YES_NO_OPTION);
 
-						if (option == 0) {
-							i = 0;
-						} else {
-							i = 1;
-						}
-					} else if (operacao.equals("-")) {
-						resultado = cb.subtrair(Double.parseDouble(n1), Double.parseDouble(n2));
-						cb.mostrarResultado((float) resultado, "Subtração");
-						int option = JOptionPane.showConfirmDialog(null, "Deseja repetir a operação?", "Aviso",
-								JOptionPane.YES_NO_OPTION);
+							if (option == 0) {
+								i = 0;
+							} else {
+								i = 1;
+							}
+						} else if (operacao.equals("-")) {
+							resultado = cb.subtrair(Double.parseDouble(n1), Double.parseDouble(n2));
+							cb.mostrarResultado((float) resultado, "Subtração");
+							int option = JOptionPane.showConfirmDialog(null, "Deseja repetir a Operação", "Aviso",
+									JOptionPane.YES_NO_OPTION);
 
-						if (option == 0) {
-							i = 0;
-						} else {
-							i = 1;
-						}
-					} else if (operacao.equals("*")) {
-						resultado = cb.multiplicar(Double.parseDouble(n1), Double.parseDouble(n2));
-						cb.mostrarResultado((float) resultado, "Multiplicação");
-						int option = JOptionPane.showConfirmDialog(null, "Deseja repetir a operação?", "Aviso",
-								JOptionPane.YES_NO_OPTION);
+							if (option == 0) {
+								i = 0;
+							} else {
+								i = 1;
+							}
+						} else if (operacao.equals("*")) {
+							resultado = cb.multiplicar(Double.parseDouble(n1), Double.parseDouble(n2));
+							cb.mostrarResultado((float) resultado, "Multiplicação");
+							int option = JOptionPane.showConfirmDialog(null, "Deseja repetir a Operação", "Aviso",
+									JOptionPane.YES_NO_OPTION);
 
-						if (option == 0) {
-							i = 0;
-						} else {
-							i = 1;
-						}
-					}
-
-					else {
-						resultado = cb.dividir(Double.parseDouble(n1), Double.parseDouble(n2));
-						cb.mostrarResultado((float) resultado, "Divisão");
-						int option = JOptionPane.showConfirmDialog(null, "Deseja repetir a operação?", "Aviso",
-								JOptionPane.YES_NO_OPTION);
-
-						if (option == 0) {
-							i = 0;
-						} else {
-							i = 1;
+							if (option == 0) {
+								i = 0;
+							} else {
+								i = 1;
+							}
 						}
 
+						else {
+							resultado = cb.dividir(Double.parseDouble(n1), Double.parseDouble(n2));
+							cb.mostrarResultado((float) resultado, "Divisão");
+							int option = JOptionPane.showConfirmDialog(null, "Deseja repetir a Operação", "Aviso",
+									JOptionPane.YES_NO_OPTION);
+
+							if (option == 0) {
+								i = 0;
+							} else {
+								i = 1;
+							}
+
+						}
+					} else {
+						JOptionPane.showMessageDialog(null, "Informe apenas números", "Erro",
+								JOptionPane.ERROR_MESSAGE);
 					}
 
 				} else {
-					JOptionPane.showMessageDialog(null, "Informe apenas números", "Erro", JOptionPane.ERROR_MESSAGE);
+					JOptionPane.showMessageDialog(null, "Informe uma operação válida!", "Erro",
+							JOptionPane.ERROR_MESSAGE);
 				}
-
 			} else {
 				JOptionPane.showMessageDialog(null, "Informe apenas números", "Erro", JOptionPane.ERROR_MESSAGE);
 			}
